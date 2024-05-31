515ff82e205d94d1da75b77c141f36601345003c
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-31 14:13:05 +1000

    created footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experince";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
@@ -17,7 +18,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
-        
+        <Footer />
       </div>
     </main>
   );
