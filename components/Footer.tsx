f414f33de9044b8e3a4f7c8fe74e775e910644ef
michael youssef <michaelyoussef396@gmail.com>
2025-01-28 16:37:42 +1100

    create blog set up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-  <footer className="w-full pt-20 pb-10 relative" id="contact">
+  <footer className="w-full pt-20 pb-10 relative bg-black-100" id="contact">
     {/* background grid */}
     <div className="w-full absolute left-0 bottom-0 min-h-96 overflow-hidden">
       <img
